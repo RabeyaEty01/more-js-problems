880f5667f2eaa703575f3f478e06be23a30aa75b
RabeyaEty01 <etyrabeya01@gmail.com>
2021-08-08 13:08:41 +0600

    add multiply

---
+++ add-multiple.js
@@ -0,0 +1,15 @@
+
+function add(num1,num2){
+    const sum= num1+num2;
+    return sum;
+}
+const firstTotal = add(63,7);
+console.log('Total sum:', firstTotal);
+
+
+function multiply(num1,num2){
+    const result =num1* num2;
+    return result;
+}
+const firstResult = multiply(12,13);
+console.log('Total multiplication:' ,firstResult);
